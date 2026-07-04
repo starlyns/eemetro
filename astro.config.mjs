c36ab6b38e015d9590ae038b1c1be6d4e1ac2081
Starlyn <admin@starlyns.com>
2026-07-04 00:01:35 -0400

    Set site URL to eemetro.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://ai.eemetro.com',
+  site: 'https://eemetro.com',
   output: 'static',
   compressHTML: true,
 });
